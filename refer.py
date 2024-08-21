95f95d01f50263225c931c79f34dd31f4612d323
Benwei Lu <2750889421@qq.com>
2024-08-21 11:27:11 +0800

    Update refer.py

--- refer.py
+++ refer.py
@@ -1,4 +1,5 @@
 __author__ = 'licheng'
+__author2__ = 'mokby'
 
 """
 This interface provides access to four datasets:
@@ -27,7 +28,7 @@ showMask   - show mask of the referred object given ref
 import sys
 import os.path as osp
 import json
-import cPickle as pickle
+import pickle
 import time
 import itertools
 import skimage.io as io
@@ -46,15 +47,16 @@ class REFER:
 		# provide data_root folder which contains refclef, refcoco, refcoco+ and refcocog
 		# also provide dataset name and splitBy information
 		# e.g., dataset = 'refcoco', splitBy = 'unc'
-		print 'loading dataset %s into memory...' % dataset
+		print('loading dataset %s into memory...' % dataset)
 		self.ROOT_DIR = osp.abspath(osp.dirname(__file__))
-		self.DATA_DIR = osp.join(data_root, dataset)
+		# self.DATA_DIR = osp.join(data_root, dataset)
+		self.DATA_DIR = data_root
 		if dataset in ['refcoco', 'refcoco+', 'refcocog']:
 			self.IMAGE_DIR = osp.join(data_root, 'images/mscoco/images/train2014')
 		elif dataset == 'refclef':
 			self.IMAGE_DIR = osp.join(data_root, 'images/saiapr_tc-12')
 		else:
-			print 'No refer dataset is called [%s]' % dataset
+			print('No refer dataset is called [%s]' % dataset)
 			sys.exit()
 
 		# load refs from data/dataset/refs(dataset).json
@@ -62,7 +64,7 @@ class REFER:
 		ref_file = osp.join(self.DATA_DIR, 'refs('+splitBy+').p')
 		self.data = {}
 		self.data['dataset'] = dataset
-		self.data['refs'] = pickle.load(open(ref_file, 'r'))
+		self.data['refs'] = pickle.load(open(ref_file, 'rb'))
 
 		# load annotations from data/dataset/instances.json
 		instances_file = osp.join(self.DATA_DIR, 'instances.json')
@@ -73,7 +75,7 @@ class REFER:
 
 		# create index
 		self.createIndex()
-		print 'DONE (t=%.2fs)' % (time.time()-tic)
+		print('DONE (t=%.2fs)' % (time.time()-tic))
 
 	def createIndex(self):
 		# create sets of mapping
@@ -89,7 +91,7 @@ class REFER:
 		# 10) catToRefs: 	{category_id: refs}
 		# 11) sentToRef: 	{sent_id: ref}
 		# 12) sentToTokens: {sent_id: tokens}
-		print 'creating index...'
+		print('creating index...')
 		# fetch info from instances
 		Anns, Imgs, Cats, imgToAnns = {}, {}, {}, {}
 		for ann in self.data['annotations']:
@@ -136,7 +138,7 @@ class REFER:
 		self.catToRefs = catToRefs
 		self.sentToRef = sentToRef
 		self.sentToTokens = sentToTokens
-		print 'index created.'
+		print('index created.')
 
 	def getRefIds(self, image_ids=[], cat_ids=[], ref_ids=[], split=''):
 		image_ids = image_ids if type(image_ids) == list else [image_ids]
@@ -164,7 +166,7 @@ class REFER:
 				elif split == 'train' or split == 'val':
 					refs = [ref for ref in refs if ref['split'] == split]
 				else:
-					print 'No such split [%s]' % split
+					print('No such split [%s]' % split)
 					sys.exit()
 		ref_ids = [ref['ref_id'] for ref in refs]
 		return ref_ids
@@ -238,7 +240,7 @@ class REFER:
 		ax.imshow(I)
 		# show refer expression
 		for sid, sent in enumerate(ref['sentences']):
-			print '%s. %s' % (sid+1, sent['sent'])
+			print('%s. %s' % (sid+1, sent['sent']))
 		# show segmentations
 		if seg_box == 'seg':
 			ann_id = ref['ann_id']
@@ -331,15 +333,16 @@ class REFER:
 
 
 if __name__ == '__main__':
-	refer = REFER(dataset='refcocog', splitBy='google')
+	# refer = REFER(data_root='your_path/refer/data',dataset='refcocog', splitBy='google')
+	refer = REFER(data_root='your_path/refer/data',dataset='refcoco', splitBy='unc')
 	ref_ids = refer.getRefIds()
 	print(len(ref_ids))
 
-	print len(refer.Imgs)
-	print len(refer.imgToRefs)
+	print(len(refer.Imgs))
+	print(len(refer.imgToRefs))
 
 	ref_ids = refer.getRefIds(split='train')
-	print 'There are %s training referred objects.' % len(ref_ids)
+	print('There are %s training referred objects.' % len(ref_ids))
 
 	for ref_id in ref_ids:
 		ref = refer.loadRefs(ref_id)[0]
@@ -347,7 +350,7 @@ if __name__ == '__main__':
 			continue
 
 		pprint(ref)
-		print 'The label is %s.' % refer.Cats[ref['category_id']]
+		print('The label is %s.' % refer.Cats[ref['category_id']])
 		plt.figure()
 		refer.showRef(ref, seg_box='box')
 		plt.show()
